560b5686978c62a244b350d075786f706d2d73f8
Thiago Zanette <thiago_zs@yahoo.com>
2019-05-20 14:51:34 -0300

    challenge solved

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+  	@ask = params[:question]
+  	if @ask == "I am going to work"
+  		@answer = "Great!"
+  	elsif @ask.include? "?"
+  		@answer = "Silly question, get dressed and go to work!"
+  	else
+  		@answer = "I don't care, get dressed and go to work!"
+  	end
+  end
+end
